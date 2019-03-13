629ba8314961d3833cdcb403b1b6823bb7a861d5
Bálint Budavölgyi <bala92x@gmail.com>
2019-03-13 08:52:19 +0100

    Change default port to 4444

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 4444;
 
 const Service = require('node-windows').Service;
 const cron = require('node-cron');
